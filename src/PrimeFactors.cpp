720cd4e675b2f2d5d2192e415dea595d4c713ef2
Rémy Milia <milia.remy@gmail.com>
2018-12-28 19:29:23 +0100

    refacto

--- src/PrimeFactors.cpp
+++ src/PrimeFactors.cpp
@@ -8,18 +8,14 @@ class PrimeFactors {
 public:
 	static list<int> generate(int value) {
 		list<int> *primes = new list<int>;
-		if (value % 2 == 0) {
-			primes->push_back(2);
-			value /= 2;
+		if (value > 1) {
+			if (value % 2 == 0) {
+				primes->push_back(2);
+				value /= 2;
+			}
 			if (value > 1) {
 				primes->push_back(value);
 			}
-		}
-
-		if (value == 2) {
-			primes->push_back(2);
-		} else if (value == 3) {
-			primes->push_back(3);
 		}
 		return *primes;
 	}
