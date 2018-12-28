b57793f176075f3e807808b723287c567ce73526
Rémy Milia <milia.remy@gmail.com>
2018-12-28 23:52:12 +0100

    make other tests cases pass

--- src/PrimeFactors.cpp
+++ src/PrimeFactors.cpp
@@ -8,13 +8,13 @@ class PrimeFactors {
 public:
 	static list<int> generate(int value) {
 		list<int> *primes = new list<int>;
-		if (value > 1) {
-			if (value % 2 == 0) {
-				primes->push_back(2);
-				value /= 2;
-			}
-			if (value > 1) {
-				primes->push_back(value);
+		int candidate = 2;
+		while (value > 1) {
+			if (value % candidate == 0) {
+				primes->push_back(candidate);
+				value /= candidate;
+			} else {
+				candidate++;
 			}
 		}
 		return *primes;
--- test/PrimeFactorsTest.cpp
+++ test/PrimeFactorsTest.cpp
@@ -25,5 +25,20 @@ TEST_CASE("should_return_a_list_with_value_2_and_2_when_generating_prime_factor_
 	CHECK(primeFactors->front() == 2);
 }
 
+TEST_CASE("should_return_a_list_with_value_3_and_3_when_generating_prime_factor_of_9") {
+	list<int> *primeFactors = new list<int>;
+	*primeFactors = PrimeFactors::generate(9);
+	CHECK(primeFactors->front() == 3);
+	CHECK(primeFactors->front() == 3);
+}
+
+TEST_CASE("should_return_a_list_with_value_2_2_2_and_2_when_generating_prime_factor_of_12") {
+	list<int> *primeFactors = new list<int>;
+	*primeFactors = PrimeFactors::generate(12);
+	CHECK(primeFactors->front() == 2);
+	CHECK(primeFactors->front() == 2);
+	CHECK(primeFactors->front() == 2);
+	CHECK(primeFactors->front() == 2);
+}
 
 
